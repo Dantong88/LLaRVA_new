c878f3e07c8126bf74cd4382881122eae090ec5e
Giscard Biamby <giscard@gmail.com>
2024-04-14 15:39:28 -0400

    Fix bug where wandb_osh throws error if wandb is not enabled

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.nn as nn
+import wandb
 
 from torch.utils.data import Sampler
 
@@ -137,7 +138,8 @@ class LengthGroupedSampler(Sampler):
 class LLaVATrainer(Trainer):
     def log(self, logs):
         super().log(logs)
-        trigger_sync()
+        if wandb.run is not None:
+            trigger_sync()
 
     def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
         if self.train_dataset is None or not has_length(self.train_dataset):
